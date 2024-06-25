83d5f14fe90206f0e4392001508972d04280b80b
Thomas Botond <tbotond.coding@gmail.com>
2024-06-25 11:20:47 +0200

    Task 3 solved: Embedded stream in chat screen.

--- lib/features/chat/chat_screen.dart
+++ lib/features/chat/chat_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:streams_exercises/features/chat/chat_repository.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
     super.key,
     required this.chatRepository,
@@ -10,13 +10,45 @@ class ChatScreen extends StatelessWidget {
   final ChatRepository chatRepository;
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Nachrichten senden starten
+    widget.chatRepository.startSendingMessages();
+  }
+
+  @override
+  void dispose() {
+    // ChatRepository aufräumen
+    widget.chatRepository.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat Screen'),
       ),
-      body: const Center(
-        child: Text("Hier sollen die Chatnachrichten stehen"),
+      body: Center(
+        child: StreamBuilder(
+            stream: widget.chatRepository.messages,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else if (!snapshot.hasData || snapshot.data == null) {
+                return const Text('Keine Nachrichten');
+              } else {
+                final message = snapshot.data!;
+                return Text('${message.user}: ${message.message}');
+              }
+            }),
       ),
     );
   }
